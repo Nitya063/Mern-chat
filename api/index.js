08e61090c4cc129efbf0326bb0f79824d211f616
Nitya063 <nityagupta510@gmail.com>
2024-01-16 21:49:19 +0530

    tenth commit

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions ={
-    origin:' http://localhost:5173', 
+    origin:'https://mernchat-nityas-projects-b607d440.vercel.app/', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
   } 
